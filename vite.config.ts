172d5c6c8ea8fbf5716c4bbe74385322b81420ef
pauloeliasc <pauloeliasc@gmail.com>
2026-06-06 15:48:25 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: '/AppJudokaDojo2/', // <-- CORRIGIDO PARA O NOME DO SEU REPOSITÓRIO
-    plugins: [
-      react(), 
-      tailwindcss(),
-    ],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
+export default defineConfig({
+  base: './',
+  plugins: [react(), tailwindcss()],
 });
